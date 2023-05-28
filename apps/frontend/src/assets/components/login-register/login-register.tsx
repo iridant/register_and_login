9011762d2ed0f07766456da69ee7aa9af1f0ace9
iridant <10984744+iridant@users.noreply.github.com>
2023-05-27 20:28:06 -0400

    CSS Updates, Add functionality to login/register mode switching

--- apps/frontend/src/assets/components/login-register/login-register.tsx
+++ apps/frontend/src/assets/components/login-register/login-register.tsx
@@ -16,6 +16,18 @@ class LoginRegister extends React.Component {
     super(props);
   }
 
+  switchMode = (button: Object) =>{
+    this.setState({mode: this.state.mode == ContainerState.Login ? ContainerState.Register : ContainerState.Login})
+  }
+
+  doLogin(){
+    console.log("Do Login")
+  }
+
+  doRegister(){
+    console.log("Do Register")
+  }
+
   render() {
     const {mode} = this.state;
     var modetext = mode ? "Register" : "Log in"
@@ -27,9 +39,9 @@ class LoginRegister extends React.Component {
             <hr/>
               <input className={styles.text_input} placeholder="Username" type="text"></input><br/>
               <input className={styles.text_input} placeholder="Password" type="password"></input><br/>
-              <button>{modetext}</button>
+              <button onClick={mode ? this.doRegister : this.doLogin} className={styles.button}>{modetext}</button>
             <hr/>
-            <button>{mode ? "Switch to Log in" : "Switch to Register"}</button>
+            <button className={styles.button} onClick={this.switchMode}>{mode ? "Switch to Log in" : "Switch to Register"}</button>
           </> 
       </div>
     )
