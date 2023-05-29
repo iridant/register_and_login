2a0699fc18f82c55fc2e726e66e23baa7911930d
iridant <10984744+iridant@users.noreply.github.com>
2023-05-28 23:16:50 -0400

    Fix bug, implement account creation, begin sign-in functionality

--- apps/backend/src/controllers/auth.controller.tsx
+++ apps/backend/src/controllers/auth.controller.tsx
@@ -2,15 +2,50 @@ const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 
 async function signUp(req, res){
-    res.status(200).send({
+
+    bcrypt.hash(req.body.password, 10).then(function(hash) {
+        User.create({ username: req.body.username, hash: hash });
+    }).catch((err) => {
+        console.error(err)
+
+        return res.status(500).send({
+            message: "Error creating user account."
+        })
+    });
+
+    return res.status(200).send({
         message: "Sign-up Success!"
     });
 }
 
 async function signIn(req, res){
-    res.status(200).send({
-        message: "Sign-in Success!"
-    });
+    try{
+        let user = User.findOne({ username: req.body.username })
+
+        if(user){
+            bcrypt.compare(req.body.password, user.hash, function(err, result) {
+                if(!result){
+                    return res.status(500).send({
+                        message: "Invalid username or password."
+                    })
+                }
+
+                return res.status(200).send({
+                    message: "Success! Logging in..."
+                })
+            });
+        }
+
+        return res.status(500).send({
+            message: "Invalid username or password."
+        })
+    }catch(err){
+        console.error(err)
+
+        return res.status(500).send({
+            message: "Error logging in."
+        })
+    }
 }
 
 async function signOut(req, res){
--- apps/backend/src/routes/auth.tsx
+++ apps/backend/src/routes/auth.tsx
@@ -4,8 +4,8 @@ const router = express.Router();
 const signupVerify = require("../middleware/signup.middleware")
 const authController = require("../controllers/auth.controller");
 
-router.post("/signin", authController.signOut);
-router.post("/signout", authController.signIn);
+router.post("/signin", authController.signIn);
+router.post("/signout", authController.signOut);
 router.post("/signup", [signupVerify.validateDuplicateUsername, signupVerify.validatePassword], authController.signUp);
 
 export {}
