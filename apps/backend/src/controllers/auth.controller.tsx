0f9e17a6bd7395b92b283c3da405a343be42e11e
iridant <10984744+iridant@users.noreply.github.com>
2023-05-29 12:29:46 -0400

    Fix JWT require statement

--- apps/backend/src/controllers/auth.controller.tsx
+++ apps/backend/src/controllers/auth.controller.tsx
@@ -1,6 +1,6 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
-const jwt = require("jwt");
+const jwt = require("jsonwebtoken");
 
 const config = require("../config/config");
 
